59fff4693503e7fccf1e3947db21e80857787442
이광기 <gwanggi.lee@gmail.com>
2020-10-31 17:27:40 +0900

    Add spring container

---
+++ src/main/java/com/jayden/HelloService.java
@@ -0,0 +1,11 @@
+package com.jayden;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class HelloService {
+
+    public String getName() {
+        return "jayden";
+    }
+}
--- src/main/java/com/jayden/HelloServlet.java
+++ src/main/java/com/jayden/HelloServlet.java
@@ -1,5 +1,8 @@
 package com.jayden;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.web.context.WebApplicationContext;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -16,11 +19,15 @@ public class HelloServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         System.out.println("DoGet");
+
+        ApplicationContext applicationContext = (ApplicationContext) getServletContext().getAttribute(WebApplicationContext.ROOT_WEB_APPLICATION_CONTEXT_ATTRIBUTE);
+        HelloService helloService = applicationContext.getBean(HelloService.class);
+
         resp.getWriter().println("<html>");
         resp.getWriter().println("<head>");
         resp.getWriter().println("</head>");
         resp.getWriter().println("<body>");
-        resp.getWriter().println("<h1>Hello, " + getName() + "</h1>");
+        resp.getWriter().println("<h1>Hello, " + helloService.getName() + "</h1>");
         resp.getWriter().println("</body>");
         resp.getWriter().println("</html>");
     }
